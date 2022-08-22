cce7c8403770928467c2f09210c8c54675eeab75
alonso garr <83713047+agarrigu@users.noreply.github.com>
2022-08-22 23:09:31 +0200

    Update displaycurrentlyplaying.sh

--- displaycurrentlyplaying.sh
+++ displaycurrentlyplaying.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if (ps -a | grep -q cmus); then
-    cmus-remote -Q | grep -E '^tag (artist|album|title)' | cut -d' ' -f 3- | sed ':a; N; $!ba; s/\n/ - /g'
+    cmus-remote -Q |
+    grep -E '^tag (artist|album|title) .+$' |
+    cut -d' ' -f 3- |
+    sed ':a; N; $!ba; s/\n/ - /g'
 else
     echo '<3' 
 fi
